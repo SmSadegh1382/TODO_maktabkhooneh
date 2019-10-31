b5883c52b8419a6ec9208851cb5aba81f64e08e2
SH Sadeghzadeh <46283990+SHSADEGH@users.noreply.github.com>
2019-10-31 16:38:41 +0330

    Updates

---
+++ precache-manifest.cbb625dc9f5cdf78426c514a4f141b69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b3b3df291c4b77f4c34",
+    "url": "/SmSadegh1382/TODO_maktabkhooneh/static/js/main.3b3b3df2.chunk.js"
+  },
+  {
+    "revision": "6e5df4a7f440c0350eb8",
+    "url": "/SmSadegh1382/TODO_maktabkhooneh/static/css/1.f176f908.chunk.css"
+  },
+  {
+    "revision": "6e5df4a7f440c0350eb8",
+    "url": "/SmSadegh1382/TODO_maktabkhooneh/static/js/1.6e5df4a7.chunk.js"
+  },
+  {
+    "revision": "9c32b2dbe865d3b579b0",
+    "url": "/SmSadegh1382/TODO_maktabkhooneh/static/js/runtime~main.9c32b2db.js"
+  },
+  {
+    "revision": "9793dcbebe121136442517737e0ccdab",
+    "url": "/SmSadegh1382/TODO_maktabkhooneh/index.html"
+  }
+];
